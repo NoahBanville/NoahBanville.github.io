c2ed6ac8aa3d087573a99731ded915bf5a59d222
NoahBanville <127630930+NoahBanville@users.noreply.github.com>
2024-03-14 19:39:12 -0400

    CV_Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const birthdateInput = document.getElementById('birthdate');
-    const ageDisplay = document.getElementById('age');
-
-    function updateAge() {
-        const birthdate = new Date(birthdateInput.value);
-        const now = new Date();
-        const diffMs = (now - birthdate);
-        const diffYears = Math.floor(diffMs / 31536000000); // years
-        const diffDays = Math.floor((diffMs % 31536000000) / 86400000); // days
-        const diffHrs = Math.floor
+nothing here
