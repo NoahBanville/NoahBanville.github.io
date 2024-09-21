66eb43b96f189317bc6325b44655046d4d3e2c2a
Noah <noban1@ulaval.ca>
2024-09-21 00:42:16 -0400

    bug_fixed

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-// Get the button element
-const downloadButton = document.getElementById('download-pdf');
+const menuBtn = document.getElementById('menu-btn');
+const menuList = document.getElementById('menu-list');
 
-// Add an event listener to the button
-downloadButton.addEventListener('click', () => {
-  // Create a new jsPDF instance
-  const pdf = new jsPDF();
-
-  // Get the HTML content of the page
-  const html = document.documentElement.outerHTML;
-
-  // Convert the HTML to a PDF
-  pdf.fromHTML(html, 15, 15, {
-    'width': 170,
-    'elementHandlers': specialElementHandlers
-  });
-
-  // Save the PDF to a file
-  const pdfBlob = pdf.output('blob');
-  const url = URL.createObjectURL(pdfBlob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'page.pdf';
-  a.click();
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('active');
 });
-
-// Special element handlers for jsPDF
-const specialElementHandlers = {
-  '#header': function(element, renderer) {
-    return true;
-  }
-};
